4c954ee6dcb6ce9516483249b4855f8c2741369e
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2022-06-14 08:39:45 +0900

    Pass tests in commit_finder_spec.rb

--- spec/esgil/commit_finder_spec.rb
+++ spec/esgil/commit_finder_spec.rb
@@ -8,12 +8,31 @@ RSpec.describe Esgil::CommitFinder do
     let(:to_branch) { 'staging' }
     let(:message) { 'test' }
 
+    let(:commit) { Git::Object::Commit.new(nil, nil) }
+    let(:commit2) { Git::Object::Commit.new(nil, nil) }
+    let(:commit3) { Git::Object::Commit.new(nil, nil) }
+    let(:commit_message) { 'test message' }
+    let(:commit_message2) { 'sample test message' }
+    let(:commit_message3) { 'sample message' }
+
+    let(:commits) { [commit, commit2, commit3] }
+
     subject { described_class.new(from_branch: from_branch, to_branch: to_branch).find(message: message) }
+
+    before do
+      allow_any_instance_of(Git::Object::Commit).to receive(:check_commit).and_return(true)
+      allow_any_instance_of(Git::Object::Commit).to receive(:between).and_return(commits)
+      allow_any_instance_of(Git::Log).to receive(:each).and_return(commits)
+
+      commit.instance_variable_set(:@message, commit_message)
+      commit2.instance_variable_set(:@message, commit_message2)
+      commit3.instance_variable_set(:@message, commit_message3)
+    end
 
     it 'get messages contains message' do
       messages = subject
-      messages.all? do |target_message|
-        target_message.contains?(message)
+      messages.each do |target_message|
+        expect(target_message.contains?(message)).to be_truthy
       end
     end
   end
