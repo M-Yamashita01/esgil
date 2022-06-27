1ac9ac58b4b4694d5bc9c777e5b0d18959e339e1
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2022-06-27 23:18:23 +0900

    Apply rubocop and resolve them.

--- lib/esgil/options.rb
+++ lib/esgil/options.rb
@@ -8,34 +8,42 @@ module Esgil
     attr_reader :to_branch
     attr_reader :message
 
-    def self.parse(command_line_args:)
-      option_parser = define_options
-      left_arguments = option_parser.parse(command_line_args)
-      if !left_arguments.empty?
-        raise ArgumentError.new("invalid option: #{left_arguments.join(',')}")
+    class << self
+      def parse(command_line_args:)
+        option_parser = define_options
+        left_arguments = option_parser.parse(command_line_args)
+        if !left_arguments.empty?
+          raise ArgumentError, "invalid option: #{left_arguments.join(',')}"
+        end
+
+        if !all_options_exist?
+          raise ArgumentError, 'from_branch, to_branch and message options are required.'
+        end
+
+        Esgil::Options.new(
+          from_branch: @options[:from_branch],
+          to_branch: @options[:to_branch],
+          message: @options[:message]
+        )
       end
 
-      if !all_options_exist?
-        raise ArgumentError.new('from_branch, to_branch and message options are required.')
+      private
+
+      def define_options
+        @options = {}
+
+        option_parser = OptionParser.new
+        option_parser.on('--from FROM_BRANCH') { |v| @options[:from_branch] = v }
+        option_parser.on('--to FROM_BRANCH') { |v| @options[:to_branch] = v }
+        option_parser.on('--message MESSAGE') { |v| @options[:message] = v }
       end
 
-      Esgil::Options.new(from_branch: @options[:from_branch], to_branch: @options[:to_branch], message: @options[:message])
+      def all_options_exist?
+        @options.key?(:from_branch) && @options.key?(:to_branch) && @options.key?(:message)
+      end
     end
 
     private
-
-    def self.define_options
-      @options = {}
-
-      option_parser = OptionParser.new
-      option_parser.on('--from FROM_BRANCH') { |v| @options[:from_branch] = v }
-      option_parser.on('--to FROM_BRANCH') { |v| @options[:to_branch] = v }
-      option_parser.on('--message MESSAGE') { |v| @options[:message] = v }
-    end
-
-    def self.all_options_exist?
-      @options.has_key?(:from_branch) && @options.has_key?(:to_branch) && @options.has_key?(:message)
-    end
 
     def initialize(from_branch:, to_branch:, message:)
       @from_branch = from_branch
